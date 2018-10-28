1e59f41a075f468fdd4dfb84ea1adba4d9426d98
Krzysztof Kośmider <kkusiu@gmail.com>
2018-10-28 22:28:31 +0100

    make function

--- make_fit_print.py
+++ make_fit_print.py
@@ -6,30 +6,32 @@ import matplotlib.pyplot as plt
 import matplotlib.dates
 from datetime import datetime
 
+def read_data(filename):
+	f= open(filename,"r")
+	date=[]; mass=[]
+	lines = f.readlines()
+	for line in lines[1:]:
+		s = line.split(',')
+		if s[11].replace('.','',1).isdigit():
+			my_date = datetime.strptime(str(s[0]), '%Y-%m-%d')
+			date.append(my_date)
+			mass.append(float(s[11]))
+	return date, mass;
+
+def plot_date(dates, mass):
+	plt.plot(date,mass)
+	plt.plot(date,mass,'+')
+	#plt.plot_damte(date,mass,'ro')
+	plt.ylabel('Masa ciala K.Kosmider [kg]')
+	plt.xlabel('Data')
+	plt.grid( True)
+	plt.xticks(rotation='vertical')
+	plt.show()
+
 
 
 script = sys.argv[0]
 filename = sys.argv[1]
-f= open(filename,"r");
 
-date = [];
-mass = [];
-lines = f.readlines();
-for line in lines[1:]:
-	s = line.split(',');
-	if s[11].replace('.','',1).isdigit():
-		my_date = datetime.strptime(str(s[0]), '%Y-%m-%d');
-		date.append(my_date);
-		mass.append(float(s[11]));
-
-plt.plot(date,mass)
-plt.plot(date,mass,'+')
-#plt.plot_damte(date,mass,'ro')
-plt.ylabel('Masa ciala K.Kosmider [kg]');
-plt.xlabel('Data');
-plt.grid(True);
-
-plt.xticks(rotation='vertical')
-
-plt.show()
-
+date, mass = read_data(filename)
+plot_date(date,mass)
