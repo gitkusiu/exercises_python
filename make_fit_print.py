338a1905889b68261c709d2755d844f3f55ad752
Krzysztof Kośmider <kkusiu@gmail.com>
2018-10-28 21:50:30 +0100

    add script to print fit data

---
+++ make_fit_print.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates
+from datetime import datetime
+
+
+
+script = sys.argv[0]
+filename = sys.argv[1]
+f= open(filename,"r");
+
+date = [];
+mass = [];
+lines = f.readlines();
+for line in lines[1:]:
+	s = line.split(',');
+	if s[11].replace('.','',1).isdigit():
+		my_date = datetime.strptime(str(s[0]), '%Y-%m-%d');
+		date.append(my_date);
+		mass.append(float(s[11]));
+
+plt.plot(date,mass)
+plt.plot(date,mass,'+')
+#plt.plot_damte(date,mass,'ro')
+plt.ylabel('Masa ciala K.Kosmider [kg]');
+plt.xlabel('Data');
+plt.grid(True);
+
+plt.xticks(rotation='vertical')
+
+plt.show()
+
